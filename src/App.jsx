85740df637381834b758bafc7702b99c5a5fe6ef
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2024-06-05 16:06:28 +0530

    Installed and configured Boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css' //React Boostrap
+
 import './App.css'
 
 import Layout from 'src/routes/Layout/Layout'
